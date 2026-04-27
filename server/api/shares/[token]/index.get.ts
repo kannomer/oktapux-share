acae9ffc3c563f51fb5fd3bb866178cc10b6b7b7
kannomer <realomrrkn@gmail.com>
2026-04-28 01:24:03 +0300

    move api endpoint

--- server/api/shares/[token]/index.get.ts
+++ server/api/shares/[token]/index.get.ts
@@ -1,5 +1,5 @@
-import { db } from '../../db/index';
-import { shares, files } from '../../db/schema';
+import { db } from '../../../db/index';
+import { shares, files } from '../../../db/schema';
 import { eq } from 'drizzle-orm';
 
 export default defineEventHandler(async (event) => {
